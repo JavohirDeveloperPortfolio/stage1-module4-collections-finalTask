1939fdc6586886173c56a1b47aa971da4d7dfe2c
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-12-28 00:20:30 +0500

    done!

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,20 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> set = new HashSet<>();
+
+        Set<String> strings = timetable.keySet();
+        for (String string : strings) {
+            for (String s : timetable.get(string)) {
+                set.add(s);
+            }
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (String s : sourceMap.keySet()) {
+            if (!map.containsKey(s.length())){
+                Set<String> temp = new HashSet<>();
+                temp.add(s);
+                map.put(s.length(),temp);
+            }else {
+                map.get(s.length()).add(s);
+                map.put(s.length(), map.get(s.length()));
+            }
+        }
+
+        return map;
     }
 }
